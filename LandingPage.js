7b7abca40b57ac04bac0da397b34eda6c72fe2ed
benson-dylan <143048807+benson-dylan@users.noreply.github.com>
2023-10-11 23:03:22 -0400

    Add files via upload

---
+++ LandingPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import NavBar from '../components/NavBar';
+
+function LandingPage()
+{
+    return (
+        <div id="LandingPageDiv">
+            <div id="NavBar">
+                <NavBar />
+            </div>
+        </div>
+    );
+}
+
+export default LandingPage;
