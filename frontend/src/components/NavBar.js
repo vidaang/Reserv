b4ff894a4c09ddc9ef2b180fa4496aa27b8f5d70
benson-dylan <143048807+benson-dylan@users.noreply.github.com>
2023-10-13 19:34:33 -0400

    NavBar functionality for LandingPage

---
+++ frontend/src/components/NavBar.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import {Navbar, Nav, Button} from 'react-bootstrap';
+import '../css/NavBar.css';
+import Logo from '../images/reserv-logo.png';
+import { Link } from 'react-router-dom';
+
+const NavBar = () => {
+    return (
+      <nav className="navbar">
+        <div className="container">
+          <a href="/">
+            <img className="brand" href="/" src={ Logo }/>
+          </a>
+          <ul className="nav-links">
+            <li className="sign-in">
+              <Link to="/Login">
+                <button className="signin-button">Sign In</button>
+              </Link>
+            </li>
+            <li className="sign-up">
+                <Link to="/Login">
+                  <button className="signup-button">Sign Up</button>
+                </Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    );
+  };
+
+export default NavBar;
