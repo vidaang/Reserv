9bf44559ea7431ec041495eff4b0796e06e58854
benson-dylan <dylan222444@gmail.com>
2023-10-13 20:40:36 -0400

    Cleaned up code

--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-
 import PageTitle from '../components/PageTitle';
 import Login from '../components/Login';
 import CreateAccount from '../components/CreateAccount';
