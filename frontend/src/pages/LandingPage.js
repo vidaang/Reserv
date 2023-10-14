0ad7bc9c5f0e417ddd858b29e2dc14bce5bd8abb
benson-dylan <dylan222444@gmail.com>
2023-10-14 14:23:40 -0400

    Changed Header Formatting and Added Buttons

--- frontend/src/pages/LandingPage.js
+++ frontend/src/pages/LandingPage.js
@@ -4,12 +4,26 @@ import '../index.css';
 
 function LandingPage()
 {
+    const handleClick = () => {
+        alert("Button clicked!");
+    }
+
     return (
         <div id="LandingPageDiv">
+            <link rel="preconnect" href="https://fonts.googleapis.com"/>
+            <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+            <link href="https://fonts.googleapis.com/css2?family=Gabarito:wght@400;700&display=swap" rel="stylesheet"></link>
             <div id="NavBar">
                 <NavBar />
             </div>
-            <h1 id="user-authetication-title">Book Your Path to Knowledge: University Room Reservations</h1>
+            <header id="landing-header">
+                <h1 id="main-title">Book Your Path to Knowledge</h1>
+                <h2 id="sub-title">University Room Reservations</h2>
+            </header>
+            <div id="landing-button-container">
+                <button id="landing-university-button" onClick={handleClick}>I'm a University</button>
+                <button id="landing-reserve-button" onClick={handleClick}>Reserve a Room</button>   
+            </div>
         </div>
     );
 }
