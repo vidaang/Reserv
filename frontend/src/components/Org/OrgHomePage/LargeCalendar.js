4681f8a020f6dce36d0e9c91592eb8930fe290cf
benson-dylan <dylan222444@gmail.com>
2023-10-21 14:55:37 -0400

    Added Mantine Drawer for Event Info Display
    On calendar page

--- frontend/src/components/Org/OrgHomePage/LargeCalendar.js
+++ frontend/src/components/Org/OrgHomePage/LargeCalendar.js
@@ -6,6 +6,10 @@ import startOfWeek from "date-fns/startOfWeek";
 import getDay from 'date-fns/getDay';
 import DatePicker from 'react-datepicker';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
+import ReservationPopUp from './ReservationPopUp';
+import { useDisclosure } from '@mantine/hooks';
+import { Drawer, Button } from '@mantine/core';
+
 const locales = {
     "en-US" : require("date-fns/locale/en-US")
 };
@@ -14,8 +18,18 @@ const events = [ // Change to Reservation
     {
         title: "General Body Meeting",
         allDay: false,
-        start: new Date(2023, 9, 20),
-        end: new Date(2023, 9, 20)
+        date: new Date(2023, 9, 20),
+        start: new Date(2023, 9, 20, 9, 0, 0),
+        end: new Date(2023, 9, 20, 10, 0, 0),
+        room: "CB2 101" 
+    },
+    {
+        title: "Tournament",
+        allDay: false,
+        date: new Date(2023, 9, 22),
+        start: new Date(2023, 9, 22, 8, 0, 0),
+        end: new Date(2023, 9, 22, 17, 0, 0),
+        room: "Pegasus Ball Room" 
     }
 ]
 
@@ -29,9 +43,23 @@ const localizer = dateFnsLocalizer({
 
 function LargeCalendar(props)
 {
-    const view = props.view;
+    const [selectedEvent, setSelectedEvent] = useState(null);
+    const [opened, { open, close }] = useDisclosure(false);
+
+    const handleEventClick = (event) => {
+        setSelectedEvent(event);
+        open();
+    };
+
     return (
-        <Calendar id="LargeCalendar" localizer={localizer} events={events} startAccessor="start" endAccessor="end" />
+        <div id="CalendarDiv">
+            <Calendar id="LargeCalendar" localizer={localizer} events={events} startAccessor="start" endAccessor="end" views={['month', 'week', 'day']} onSelectEvent={handleEventClick}/>
+            <Drawer opened={opened} onClose={close} position="right" overlayProps={{ backgroundOpacity: 0.1, blur: 4 }}>
+                {selectedEvent && (
+                    <ReservationPopUp event={selectedEvent} />
+                )}
+            </Drawer>
+        </div>
     );
 }
 
