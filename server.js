fdbe0984ddca3e85efcbb01857ac3b725bdb1c28
StudentJamesChen <jameselichen@gmail.com>
2023-10-28 20:00:08 -0400

    Availibility get request end point.

--- server.js
+++ server.js
@@ -227,6 +227,37 @@ app.post("/api/createEvent", async (req, res) => {
   }
 });
 
+// Example code for how to return availbility, need to see how to use this URL endpoint
+app.get("/api/availability/:roomID/:date", async (req, res) => {
+  const roomID = parseInt(req.params.roomID); // Convert RoomID to integer
+  const day = req.params.date; // Date in "YYYY-MM-DD" format
+
+  const db = client.db("Reserv");
+
+  // Fetch events for the specified day and room from the database
+  const eventsOnDay = await db
+    .collection("Events")
+    .find({ RoomID: roomID, Date: day })
+    .toArray();
+
+  // Initialize availability array with all true values
+  // availability[0] being true means 9:00am to 9:30am is available.
+  const availability = new Array(24).fill(true);
+
+  eventsOnDay.forEach((event) => {
+    // Calculate indices based on start and end times
+    const startIndex = (event.StartEnd[0] - 9.0) * 2; // *2 because we're considering half-hour intervals
+    const endIndex = (event.StartEnd[1] - 9.0) * 2;
+
+    // Mark the hours between start and end times as false (unavailable)
+    for (let i = startIndex; i <= endIndex; i++) {
+      availability[i] = false;
+    }
+  });
+
+  res.status(200).json({ availability: availability });
+});
+
 // PUT NEW APIs BEFORE HERE
 
 // This needs to be the last get request.
@@ -238,149 +269,3 @@ if (process.env.NODE_ENV === "production") {
     res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
   });
 }
-// -------------- Boiler Plate Code from MERN Stack Demo--------------- //
-
-var cardList = [
-  "Roy Campanella",
-  "Paul Molitor",
-  "Tony Gwynn",
-  "Dennis Eckersley",
-  "Reggie Jackson",
-  "Gaylord Perry",
-  "Buck Leonard",
-  "Rollie Fingers",
-  "Charlie Gehringer",
-  "Wade Boggs",
-  "Carl Hubbell",
-  "Dave Winfield",
-  "Jackie Robinson",
-  "Ken Griffey, Jr.",
-  "Al Simmons",
-  "Chuck Klein",
-  "Mel Ott",
-  "Mark McGwire",
-  "Nolan Ryan",
-  "Ralph Kiner",
-  "Yogi Berra",
-  "Goose Goslin",
-  "Greg Maddux",
-  "Frankie Frisch",
-  "Ernie Banks",
-  "Ozzie Smith",
-  "Hank Greenberg",
-  "Kirby Puckett",
-  "Bob Feller",
-  "Dizzy Dean",
-  "Joe Jackson",
-  "Sam Crawford",
-  "Barry Bonds",
-  "Duke Snider",
-  "George Sisler",
-  "Ed Walsh",
-  "Tom Seaver",
-  "Willie Stargell",
-  "Bob Gibson",
-  "Brooks Robinson",
-  "Steve Carlton",
-  "Joe Medwick",
-  "Nap Lajoie",
-  "Cal Ripken, Jr.",
-  "Mike Schmidt",
-  "Eddie Murray",
-  "Tris Speaker",
-  "Al Kaline",
-  "Sandy Koufax",
-  "Willie Keeler",
-  "Pete Rose",
-  "Robin Roberts",
-  "Eddie Collins",
-  "Lefty Gomez",
-  "Lefty Grove",
-  "Carl Yastrzemski",
-  "Frank Robinson",
-  "Juan Marichal",
-  "Warren Spahn",
-  "Pie Traynor",
-  "Roberto Clemente",
-  "Harmon Killebrew",
-  "Satchel Paige",
-  "Eddie Plank",
-  "Josh Gibson",
-  "Oscar Charleston",
-  "Mickey Mantle",
-  "Cool Papa Bell",
-  "Johnny Bench",
-  "Mickey Cochrane",
-  "Jimmie Foxx",
-  "Jim Palmer",
-  "Cy Young",
-  "Eddie Mathews",
-  "Honus Wagner",
-  "Paul Waner",
-  "Grover Alexander",
-  "Rod Carew",
-  "Joe DiMaggio",
-  "Joe Morgan",
-  "Stan Musial",
-  "Bill Terry",
-  "Rogers Hornsby",
-  "Lou Brock",
-  "Ted Williams",
-  "Bill Dickey",
-  "Christy Mathewson",
-  "Willie McCovey",
-  "Lou Gehrig",
-  "George Brett",
-  "Hank Aaron",
-  "Harry Heilmann",
-  "Walter Johnson",
-  "Roger Clemens",
-  "Ty Cobb",
-  "Whitey Ford",
-  "Willie Mays",
-  "Rickey Henderson",
-  "Babe Ruth",
-];
-
-app.post("/api/addcard", async (req, res, next) => {
-  // incoming: userId, color
-  // outgoing: error
-  const { userId, card } = req.body;
-  const newCard = { Card: card, UserId: userId };
-  var error = "";
-
-  try {
-    const db = client.db("COP4331Cards");
-    const result = db.collection("Cards").insertOne(newCard);
-  } catch (e) {
-    error = e.toString();
-  }
-
-  cardList.push(card);
-  var ret = { error: error };
-  res.status(200).json(ret);
-});
-
-app.post("/api/searchcards", async (req, res) => {
-  // incoming: userId, search
-  // outgoing: results[], error
-  var error = "";
-  const { userId, search } = req.body;
-  var _search = search.trim();
-
-  const db = client.db("COP4331Cards");
-  const results = await db
-    .collection("Cards")
-    .find({ Card: { $regex: _search + ".*", $options: "i" }, UserId: userId })
-    .toArray();
-  var _ret = [];
-
-  for (var i = 0; i < results.length; i++) {
-    _ret.push(results[i].Card);
-  }
-
-  var ret = { results: _ret, error: error };
-  res.status(200).json(ret);
-});
-
-//----------------------------------------------
