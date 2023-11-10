70708d9d9ac5d3af4ebb74cde53b0542eeb6981c
EdwardWeir01 <93448600+EdwardWeir01@users.noreply.github.com>
2023-11-10 16:21:10 -0500

    Update login.dart
    Added styling and "sign up here" button to the login page

--- frontend_mobile/reserv_mobile_app/lib/screens/login.dart
+++ frontend_mobile/reserv_mobile_app/lib/screens/login.dart
@@ -1,17 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:reserv_mobile_app/screens/signup.dart';
 import '../services/api_service.dart';
-import 'home.dart'; 
+import 'home.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Login'),
-      ),
-      body: const LoginForm(),
+    return const Scaffold(
+      backgroundColor: Color(0xFF88E19C),
+      body: LoginForm(),
     );
   }
 }
@@ -31,63 +31,208 @@ class _LoginFormState extends State<LoginForm> {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(16.0),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
+      padding: const EdgeInsets.fromLTRB(42, 214, 42, 0),
+      child: Center(
+          child: Column(
         children: <Widget>[
-          TextFormField(
-            controller: _emailController,
-            decoration: const InputDecoration(labelText: 'Email'),
-          ),
-          const SizedBox(height: 16.0),
-          TextFormField(
-            controller: _passController,
-            obscureText: true,
-            decoration: const InputDecoration(labelText: 'Password'),
-          ),
-          const SizedBox(height: 24.0),
-          
-          ElevatedButton(
-            onPressed: () async {
-              // Validate the form
-              if (_emailController.text.isEmpty || _passController.text.isEmpty) {
-                setState(() {
-                  _message = 'Please enter both email and password';
-                });
-                return;
-              }
-              else {
+          FittedBox(
+              child: Container(
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      border: Border.all(
+                        color: Colors.white,
+                      ),
+                      borderRadius:
+                          const BorderRadius.all(Radius.circular(23))),
+                  width: 306.0,
+                  height: 416.0,
+                  child: Column(
+                    children: <Widget>[
+                      const SizedBox(height: 17.0),
+                      Text(
+                        'Sign In',
+                        style: GoogleFonts.rubik(
+                          textStyle: const TextStyle(
+                            fontSize: 32,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 16.0),
+                      Align(
+                        alignment: const Alignment(-.6, 0),
+                        child: SizedBox(
+                          width: 103,
+                          height: 15,
+                          child: Text(
+                            'Username',
+                            style: GoogleFonts.rubik(
+                              textStyle: const TextStyle(
+                                fontSize: 14,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 4.0),
+                      SizedBox(
+                        width: 262,
+                        height: 28,
+                        child: TextFormField(
+                          decoration: InputDecoration(
+                            enabledBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(53.0),
+                                borderSide:
+                                    const BorderSide(color: Color(0xFFDFDFDF))),
+                          ),
+                          controller: _emailController,
+                        ),
+                      ),
+                      const SizedBox(height: 16.0),
+                      Align(
+                        alignment: const Alignment(-.6, 0),
+                        child: SizedBox(
+                          width: 103,
+                          height: 15,
+                          child: Text(
+                            'Password',
+                            style: GoogleFonts.rubik(
+                              textStyle: const TextStyle(
+                                fontSize: 14,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 4.0),
+                      SizedBox(
+                        width: 262,
+                        height: 28,
+                        child: TextFormField(
+                          decoration: InputDecoration(
+                            enabledBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(53.0),
+                                borderSide:
+                                    const BorderSide(color: Color(0xFFDFDFDF))),
+                          ),
+                          controller: _passController,
+                          obscureText: true,
+                        ),
+                      ),
+                      const SizedBox(height: 47.0),
+                      SizedBox(
+                        width: 186,
+                        height: 32,
+                        child: TextButton(
+                          style: ButtonStyle(
+                              shape: MaterialStateProperty.all<
+                                      RoundedRectangleBorder>(
+                                  RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(36.0),
+                                      side: const BorderSide(
+                                          color: Color(0xFFDFDFDF))))),
+                          onPressed: () async {
+                            // Validate the form
+                            if (_emailController.text.isEmpty ||
+                                _passController.text.isEmpty) {
+                              setState(() {
+                                _message =
+                                    'Please enter both email and password';
+                              });
+                              return;
+                            } else {
+                              try {
+                                final response = await ApiService.login(
+                                  _emailController.text,
+                                  _passController.text,
+                                );
 
-                try {
-                  final response = await ApiService.login(
-                    _emailController.text,
-                    _passController.text,
-                  );
-
-                  if (response['error'] == null) {
-                    // Login was successful
-                    Navigator.of(context).pushReplacement(
-                      MaterialPageRoute(builder: (context) => const HomePage()),
-                    );
-                  } else {
-                    // Login failed, show an error message
-                    setState(() {
-                      _message = 'Login failed: ${response['error']}';
-                    });
-                  }
-                } catch (e) {
-                  // Handle network or other errors
-                  setState(() {
-                    _message = 'An error occurred: $e';
-                  });
-                }
-              }
-            },
-            child: const Text('Log In'),
-          ),
-          Text(_message, style: const TextStyle(color: Colors.red),),
+                                if (response['error'] == null) {
+                                  // Login was successful
+                                  Navigator.of(context).pushReplacement(
+                                    MaterialPageRoute(
+                                        builder: (context) => const HomePage()),
+                                  );
+                                } else {
+                                  // Login failed, show an error message
+                                  setState(() {
+                                    _message =
+                                        'Login failed: ${response['error']}';
+                                  });
+                                }
+                              } catch (e) {
+                                // Handle network or other errors
+                                setState(() {
+                                  _message = 'An error occurred: $e';
+                                });
+                              }
+                            }
+                          },
+                          child: Text(
+                            'Login',
+                            style: GoogleFonts.rubik(
+                              textStyle: const TextStyle(
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.black),
+                            ),
+                          ),
+                        ),
+                      ),
+                      Text(
+                        _message,
+                        style: const TextStyle(color: Colors.red),
+                      ),
+                      const SizedBox(height: 5.0),
+                      SizedBox(
+                        width: 186,
+                        height: 32,
+                        child: TextButton(
+                          style: ButtonStyle(
+                              shape: MaterialStateProperty.all<
+                                      RoundedRectangleBorder>(
+                                  RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(36.0),
+                                      side: const BorderSide(
+                                          color: Color(0xFFDFDFDF))))),
+                          onPressed: () {
+                            // Navigate to the SignUpScreen when the "Sign Up" button is pressed.
+                            Navigator.of(context).push(
+                              MaterialPageRoute(
+                                  builder: (context) => const SignUpPage()),
+                            );
+                          },
+                          child: Text(
+                            'Sign Up Here',
+                            style: GoogleFonts.rubik(
+                              textStyle: const TextStyle(
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.black),
+                            ),
+                          ),
+                        ),
+                      ),
+                      // ElevatedButton(
+                      //   onPressed: () {
+                      //     // Navigate to the SignUpScreen when the "Sign Up" button is pressed.
+                      //     Navigator.of(context).push(
+                      //       MaterialPageRoute(
+                      //           builder: (context) => const SignUpPage()),
+                      //     );
+                      //   },
+                      //   child: const Text('Sign Up Here'),
+                      //   style: ElevatedButton.styleFrom(
+                      //     primary: Color(0xFF526760), // Button color
+                      //     minimumSize: Size(200, 50), // Button size
+                      //   ),
+                      // ),
+                    ],
+                  ))),
         ],
-      ),
+      )),
     );
   }
 }
