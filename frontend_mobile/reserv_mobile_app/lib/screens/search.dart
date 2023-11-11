523192711d8e45fe9eb4753c14ff87bea562b0d3
vidaang <94209946+vidaang@users.noreply.github.com>
2023-11-11 18:19:26 -0500

    Search

--- frontend_mobile/reserv_mobile_app/lib/screens/search.dart
+++ frontend_mobile/reserv_mobile_app/lib/screens/search.dart
@@ -1,22 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-class SearchPage extends StatelessWidget { 
-  const SearchPage({Key? key}) : super(key: key); 
-  
-  @override 
-  Widget build(BuildContext context) { 
-    return Container( 
-      color: const Color(0xffC4DFCB), 
-      child: Center( 
-        child: Text( 
-          "Search Page", 
-          style: TextStyle( 
-            color: Colors.green[900], 
-            fontSize: 45, 
-            fontWeight: FontWeight.w500, 
-          ), 
-        ), 
-      ), 
-    ); 
-  } 
+
+class SearchPage extends StatefulWidget {
+  const SearchPage({Key? key}) : super(key: key);
+
+  @override
+  _SearchPageState createState() => _SearchPageState();
+}
+
+class _SearchPageState extends State<SearchPage> {
+  static const _initialCameraPosition = CameraPosition(
+    target: LatLng(28.602333068847656, -81.20020294189453),
+    zoom: 11.5,
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: GoogleMap(
+        initialCameraPosition: _initialCameraPosition,
+        onMapCreated: (GoogleMapController controller) {
+          // You can use the controller to interact with the map
+        },
+      ),
+    );
+  }
 }
