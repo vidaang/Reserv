d4491b6a48bc9f5dcf170cf283d49c6387cf90af
EdwardWeir01 <ejweir0001@gmail.com>
2023-11-27 12:01:02 -0500

    Updated Settings main page

--- frontend_mobile/reserv_mobile_app/lib/screens/settings.dart
+++ frontend_mobile/reserv_mobile_app/lib/screens/settings.dart
@@ -1,4 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:reserv_mobile_app/screens/signup.dart';
+import '../services/api_service.dart';
+import '../widgets/navbar.dart';
+import '../services/jwt_token.dart';
+import '../screens/complete_profile.dart';
+
 
 class SettingsPage extends StatelessWidget { 
   const SettingsPage({Key? key}) : super(key: key); 
@@ -6,17 +14,139 @@ class SettingsPage extends StatelessWidget {
   @override 
   Widget build(BuildContext context) { 
     return Container( 
-      color: const Color(0xffC4DFCB), 
-      child: Center( 
-        child: Text( 
-          "Settings Page", 
-          style: TextStyle( 
-            color: Colors.green[900], 
-            fontSize: 45, 
-            fontWeight: FontWeight.w500, 
-          ), 
-        ), 
-      ), 
-    ); 
+      color: Colors.white, 
+      child: SingleChildScrollView(
+        child: Center( 
+        child: Column(
+        children: <Widget>[ 
+          const SizedBox(height: 31.0),
+          
+          Text(
+            'Settings',
+            style: GoogleFonts.rubik(
+                  textStyle: const TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                  ),
+            
+          )), 
+           const SizedBox(height: 15.0),
+            FittedBox(
+              child: Container(
+                decoration: BoxDecoration(
+                      color: Color(0xFFF2F2F2),
+                      border: Border.all(
+                        color: Color(0xFFF2F2F2),
+                      ),
+                      borderRadius:
+                          const BorderRadius.all(Radius.circular(11))),
+                  width: 310.0,
+                  height: 410.0,
+                  child: Column(
+                    children: <Widget>[
+                      const SizedBox(height: 16.0),
+          SizedBox(
+                        width: 272,
+                        height: 60,
+                        child: TextButton(
+                          
+                          onPressed: () async {
+                            
+                          },
+                          child: Text(
+                            'Organization Information',
+                            style: GoogleFonts.rubik(
+                              textStyle: const TextStyle(
+                                  fontSize: 19,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.black),
+                            ))),
+                          ),
+                          Divider(
+                  color: Colors.black,
+                ),
+                SizedBox(
+                        width: 272,
+                        height: 60,
+                        child: TextButton(
+                          
+                          onPressed: () async {
+                            
+                          },
+                          child: Text(
+                            'University Information',
+                            style: GoogleFonts.rubik(
+                              textStyle: const TextStyle(
+                                  fontSize: 19,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.black),
+                            ))),
+                          ),
+                          Divider(
+                  color: Colors.black,
+                ),
+                SizedBox(
+                        width: 272,
+                        height: 60,
+                        child: TextButton(
+                          
+                          onPressed: () async {
+                            
+                          },
+                          child: Text(
+                            'Notifications',
+                            style: GoogleFonts.rubik(
+                              textStyle: const TextStyle(
+                                  fontSize: 19,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.black),
+                            ))),
+                          ),
+                          Divider(
+                  color: Colors.black,
+                ),
+                SizedBox(
+                        width: 272,
+                        height: 60,
+                        child: TextButton(
+                          
+                          onPressed: () async {
+                            
+                          },
+                          child: Text(
+                            'Login Information',
+                            style: GoogleFonts.rubik(
+                              textStyle: const TextStyle(
+                                  fontSize: 19,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.black),
+                            ))),
+                          ),
+                          Divider(
+                  color: Colors.black,
+                ),
+                SizedBox(
+                        width: 272,
+                        height: 60,
+                        child: TextButton(
+                          
+                          onPressed: () async {
+                            
+                          },
+                          child: Text(
+                            'Other Settings',
+                            style: GoogleFonts.rubik(
+                              textStyle: const TextStyle(
+                                  fontSize: 19,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.black),
+                            ))),
+                          ),
+                          ],
+                        ),
+            )),
+      ]), 
+    ))); 
   } 
 }
+
