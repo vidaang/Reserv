db3f8d4d24450d8013bbc0ea3cd8bde6c238f37d
EdwardWeir01 <ejweir0001@gmail.com>
2023-11-27 18:31:19 -0500

    Added pop up windows for buttons on settings

--- frontend_mobile/reserv_mobile_app/lib/screens/settings.dart
+++ frontend_mobile/reserv_mobile_app/lib/screens/settings.dart
@@ -51,7 +51,10 @@ class SettingsPage extends StatelessWidget {
                         child: TextButton(
                           
                           onPressed: () async {
-                            
+                            showDialog(
+              context: context,
+              builder: (BuildContext context) => _buildPopupDialogOrganizationInformation(context),
+            );
                           },
                           child: Text(
                             'Organization Information',
@@ -62,7 +65,7 @@ class SettingsPage extends StatelessWidget {
                                   color: Colors.black),
                             ))),
                           ),
-                          Divider(
+                          const Divider(
                   color: Colors.black,
                 ),
                 SizedBox(
@@ -71,7 +74,10 @@ class SettingsPage extends StatelessWidget {
                         child: TextButton(
                           
                           onPressed: () async {
-                            
+                            showDialog(
+              context: context,
+              builder: (BuildContext context) => _buildPopupDialogUniversityInformation(context),
+            );
                           },
                           child: Text(
                             'University Information',
@@ -82,7 +88,7 @@ class SettingsPage extends StatelessWidget {
                                   color: Colors.black),
                             ))),
                           ),
-                          Divider(
+                          const Divider(
                   color: Colors.black,
                 ),
                 SizedBox(
@@ -91,7 +97,10 @@ class SettingsPage extends StatelessWidget {
                         child: TextButton(
                           
                           onPressed: () async {
-                            
+                            showDialog(
+              context: context,
+              builder: (BuildContext context) => _buildPopupDialogNotifications(context),
+            );
                           },
                           child: Text(
                             'Notifications',
@@ -102,7 +111,7 @@ class SettingsPage extends StatelessWidget {
                                   color: Colors.black),
                             ))),
                           ),
-                          Divider(
+                          const Divider(
                   color: Colors.black,
                 ),
                 SizedBox(
@@ -111,7 +120,10 @@ class SettingsPage extends StatelessWidget {
                         child: TextButton(
                           
                           onPressed: () async {
-                            
+                            showDialog(
+              context: context,
+              builder: (BuildContext context) => _buildPopupDialogLoginInformation(context),
+            );
                           },
                           child: Text(
                             'Login Information',
@@ -122,7 +134,7 @@ class SettingsPage extends StatelessWidget {
                                   color: Colors.black),
                             ))),
                           ),
-                          Divider(
+                          const Divider(
                   color: Colors.black,
                 ),
                 SizedBox(
@@ -131,7 +143,10 @@ class SettingsPage extends StatelessWidget {
                         child: TextButton(
                           
                           onPressed: () async {
-                            
+                            showDialog(
+              context: context,
+              builder: (BuildContext context) => _buildPopupDialogOtherSettings(context),
+            );
                           },
                           child: Text(
                             'Other Settings',
@@ -148,5 +163,135 @@ class SettingsPage extends StatelessWidget {
       ]), 
     ))); 
   } 
+
+  Widget _buildPopupDialogOrganizationInformation(BuildContext context) {
+  return AlertDialog(
+    title: Text('Organization Information', style: GoogleFonts.rubik(
+                              textStyle: TextStyle(
+                                  fontSize: 19,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.black),)),
+    content: Column(
+      mainAxisSize: MainAxisSize.min,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Text("Hello"),
+      ],
+    ),
+    actions: <Widget>[
+       TextButton(
+        onPressed: () {
+          Navigator.of(context).pop();
+        },
+        //textColor: Theme.of(context).primaryColor,
+        child: const Text('Close'),
+      ),
+    ],
+  );
+}
+
+Widget _buildPopupDialogUniversityInformation(BuildContext context) {
+  return AlertDialog(
+    title: Text('University Information', style: GoogleFonts.rubik(
+                              textStyle: TextStyle(
+                                  fontSize: 19,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.black),)),
+    content: Column(
+      mainAxisSize: MainAxisSize.min,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Text("Hello"),
+      ],
+    ),
+    actions: <Widget>[
+       TextButton(
+        onPressed: () {
+          Navigator.of(context).pop();
+        },
+        //textColor: Theme.of(context).primaryColor,
+        child: const Text('Close'),
+      ),
+    ],
+  );
+}
+
+Widget _buildPopupDialogNotifications(BuildContext context) {
+  return AlertDialog(
+    title: Text('Notifications', style: GoogleFonts.rubik(
+                              textStyle: TextStyle(
+                                  fontSize: 19,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.black),)),
+    content: Column(
+      mainAxisSize: MainAxisSize.min,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Text("Hello"),
+      ],
+    ),
+    actions: <Widget>[
+       TextButton(
+        onPressed: () {
+          Navigator.of(context).pop();
+        },
+        //textColor: Theme.of(context).primaryColor,
+        child: const Text('Close'),
+      ),
+    ],
+  );
+}
+
+Widget _buildPopupDialogLoginInformation(BuildContext context) {
+  return AlertDialog(
+    title: Text('Login Information', style: GoogleFonts.rubik(
+                              textStyle: TextStyle(
+                                  fontSize: 19,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.black),)),
+    content: Column(
+      mainAxisSize: MainAxisSize.min,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Text("Hello"),
+      ],
+    ),
+    actions: <Widget>[
+       TextButton(
+        onPressed: () {
+          Navigator.of(context).pop();
+        },
+        //textColor: Theme.of(context).primaryColor,
+        child: const Text('Close'),
+      ),
+    ],
+  );
+}
+
+Widget _buildPopupDialogOtherSettings(BuildContext context) {
+  return AlertDialog(
+    title: Text('Other Settings', style: GoogleFonts.rubik(
+                              textStyle: TextStyle(
+                                  fontSize: 19,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.black),)),
+    content: Column(
+      mainAxisSize: MainAxisSize.min,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Text("Hello"),
+      ],
+    ),
+    actions: <Widget>[
+       TextButton(
+        onPressed: () {
+          Navigator.of(context).pop();
+        },
+        //textColor: Theme.of(context).primaryColor,
+        child: const Text('Close'),
+      ),
+    ],
+  );
+}
 }
 
