6bac096975379c858910775458cf084c0c9bc59a
EdwardWeir01 <ejweir0001@gmail.com>
2023-11-27 23:46:57 -0500

    settings update

--- frontend_mobile/reserv_mobile_app/lib/screens/settings.dart
+++ frontend_mobile/reserv_mobile_app/lib/screens/settings.dart
@@ -9,8 +9,15 @@ import '../screens/complete_profile.dart';
 
 
 class SettingsPage extends StatelessWidget { 
-  const SettingsPage({Key? key}) : super(key: key); 
+   SettingsPage({Key? key}) : super(key: key); 
   
+  String _message = '';
+
+  // final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final TextEditingController _EmailController = TextEditingController();
+  final TextEditingController _passController = TextEditingController();
+  final TextEditingController _retypePassController = TextEditingController();
+
   @override 
   Widget build(BuildContext context) { 
     return Container( 
@@ -34,28 +41,21 @@ class SettingsPage extends StatelessWidget {
             FittedBox(
               child: Container(
                 decoration: BoxDecoration(
-                      color: Color(0xFFF2F2F2),
+                      color: const Color(0xFFF2F2F2),
                       border: Border.all(
-                        color: Color(0xFFF2F2F2),
+                        color: const Color(0xFFF2F2F2),
                       ),
                       borderRadius:
                           const BorderRadius.all(Radius.circular(11))),
                   width: 310.0,
-                  height: 410.0,
+                  height: 710.0,
                   child: Column(
                     children: <Widget>[
                       const SizedBox(height: 16.0),
           SizedBox(
                         width: 272,
                         height: 60,
-                        child: TextButton(
-                          
-                          onPressed: () async {
-                            showDialog(
-              context: context,
-              builder: (BuildContext context) => _buildPopupDialogOrganizationInformation(context),
-            );
-                          },
+                        
                           child: Text(
                             'Organization Information',
                             style: GoogleFonts.rubik(
@@ -64,67 +64,16 @@ class SettingsPage extends StatelessWidget {
                                   fontWeight: FontWeight.bold,
                                   color: Colors.black),
                             ))),
-                          ),
+                          
                           const Divider(
                   color: Colors.black,
                 ),
+                
+                
                 SizedBox(
                         width: 272,
                         height: 60,
-                        child: TextButton(
-                          
-                          onPressed: () async {
-                            showDialog(
-              context: context,
-              builder: (BuildContext context) => _buildPopupDialogUniversityInformation(context),
-            );
-                          },
-                          child: Text(
-                            'University Information',
-                            style: GoogleFonts.rubik(
-                              textStyle: const TextStyle(
-                                  fontSize: 19,
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.black),
-                            ))),
-                          ),
-                          const Divider(
-                  color: Colors.black,
-                ),
-                SizedBox(
-                        width: 272,
-                        height: 60,
-                        child: TextButton(
-                          
-                          onPressed: () async {
-                            showDialog(
-              context: context,
-              builder: (BuildContext context) => _buildPopupDialogNotifications(context),
-            );
-                          },
-                          child: Text(
-                            'Notifications',
-                            style: GoogleFonts.rubik(
-                              textStyle: const TextStyle(
-                                  fontSize: 19,
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.black),
-                            ))),
-                          ),
-                          const Divider(
-                  color: Colors.black,
-                ),
-                SizedBox(
-                        width: 272,
-                        height: 60,
-                        child: TextButton(
-                          
-                          onPressed: () async {
-                            showDialog(
-              context: context,
-              builder: (BuildContext context) => _buildPopupDialogLoginInformation(context),
-            );
-                          },
+                        
                           child: Text(
                             'Login Information',
                             style: GoogleFonts.rubik(
@@ -133,7 +82,7 @@ class SettingsPage extends StatelessWidget {
                                   fontWeight: FontWeight.bold,
                                   color: Colors.black),
                             ))),
-                          ),
+                        
                           
                 
                           ],
@@ -141,110 +90,7 @@ class SettingsPage extends StatelessWidget {
             )),
       ]), 
     ))); 
-  } 
-
-  Widget _buildPopupDialogOrganizationInformation(BuildContext context) {
-  return AlertDialog(
-    title: Text('Organization Information', style: GoogleFonts.rubik(
-                              textStyle: TextStyle(
-                                  fontSize: 19,
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.black),)),
-    content: Column(
-      mainAxisSize: MainAxisSize.min,
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: <Widget>[
-        Text("Hello"),
-      ],
-    ),
-    actions: <Widget>[
-       TextButton(
-        onPressed: () {
-          Navigator.of(context).pop();
-        },
-        //textColor: Theme.of(context).primaryColor,
-        child: const Text('Close'),
-      ),
-    ],
-  );
-}
-
-Widget _buildPopupDialogUniversityInformation(BuildContext context) {
-  return AlertDialog(
-    title: Text('University Information', style: GoogleFonts.rubik(
-                              textStyle: TextStyle(
-                                  fontSize: 19,
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.black),)),
-    content: Column(
-      mainAxisSize: MainAxisSize.min,
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: <Widget>[
-        Text("Hello"),
-      ],
-    ),
-    actions: <Widget>[
-       TextButton(
-        onPressed: () {
-          Navigator.of(context).pop();
-        },
-        //textColor: Theme.of(context).primaryColor,
-        child: const Text('Close'),
-      ),
-    ],
-  );
-}
-
-Widget _buildPopupDialogNotifications(BuildContext context) {
-  return AlertDialog(
-    title: Text('Notifications', style: GoogleFonts.rubik(
-                              textStyle: TextStyle(
-                                  fontSize: 19,
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.black),)),
-    content: Column(
-      mainAxisSize: MainAxisSize.min,
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: <Widget>[
-        Text("Hello"),
-      ],
-    ),
-    actions: <Widget>[
-       TextButton(
-        onPressed: () {
-          Navigator.of(context).pop();
-        },
-        //textColor: Theme.of(context).primaryColor,
-        child: const Text('Close'),
-      ),
-    ],
-  );
-}
-
-Widget _buildPopupDialogLoginInformation(BuildContext context) {
-  return AlertDialog(
-    title: Text('Login Information', style: GoogleFonts.rubik(
-                              textStyle: TextStyle(
-                                  fontSize: 19,
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.black),)),
-    content: Column(
-      mainAxisSize: MainAxisSize.min,
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: <Widget>[
-        Text("Hello"),
-      ],
-    ),
-    actions: <Widget>[
-       TextButton(
-        onPressed: () {
-          Navigator.of(context).pop();
-        },
-        //textColor: Theme.of(context).primaryColor,
-        child: const Text('Close'),
-      ),
-    ],
-  );
-}
+  
+  }
 }
 
