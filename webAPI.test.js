93694253498b78778f25c76e246277761cac7966
Sean Feldman <the.feldman23@gmail.com>
2023-11-29 00:05:33 -0500

    Added 3 unit tests!

--- webAPI.test.js
+++ webAPI.test.js
@@ -33,34 +33,59 @@ afterAll(async () => {
 });
 
 describe('API Tests', () => {
-    test('PUT /api/createRSO', async () => {
+    test('GET /api/GetUniInfo', async () => {
 
         const requestBody = {
-            RSOID: new ObjectId(),
-            Email: "unitTest",
-            Password: "unitTest",
-            RSOName: "unitTest",
-            OfficerFirstName: "unitTest",
-            OfficerLastName: "unitTest",
-            OfficerEmail: "unitTest",
-            Phone: "unitTest",
-            AdvisorName: "unitTest",
-            AdvisorEmail: "unitTest",
-            SecondaryContactName: "unitTest",
-            SecondaryContactEmail: "unitTest",
-            SecondaryContactPhone: "unitTest",
-            EmailVerification: false,
-            Verification: false,
-            UniID: new ObjectId(UniID),
+            "UniversityID": new ObjectId("6566be76dd46c9561a7f8419")
         }
 
-        const response = await request(app)
-            .put('/api/createRSO')
-            .send(requestBody);
+        const response = await request(app).post('/api/getUniInfo').send(requestBody)
 
         expect(response.statusCode).toBe(200);
+        expect(response.body.UniName).toBe("unitTest");
+        expect(response.body.Address).toBe("unitTest");
+        expect(response.body.EmailDomain).toBe("unitTest");
+        expect(response.body.Website).toBe("unitTest");
+        expect(response.body.Phone).toBe("unitTest");
         // additional assertions
     });
+
+    test('GET /api/RetrieveBuildings', async () => {
+        const requestBody = {
+            "Latitude": 0,
+            "Longitude": 0
+        }
+
+        const response = await request(app).post('/api/RetrieveRooms').send(requestBody)
+        console.log(response.body)
+        expect(response.statusCode).toBe(200);
+        expect(response.body.roomList[0].RoomNumber).toBe(0)
+        expect(response.body.roomList[0].RoomType).toBe("unitTest")
+        expect(response.body.roomList[0].RoomInfo).toBe("unitTest")
+        expect(response.body.roomList[0].UniID).toBe("6566be76dd46c9561a7f8419")
+
+    })
+
+    test('GET /api/MultipleRetrieveBuildings', async () => {
+        const requestBody = {
+            "Latitude": 0,
+            "Longitude": 0
+        }
+
+        const response = await request(app).post('/api/RetrieveRooms').send(requestBody)
+        console.log(response.body)
+        expect(response.statusCode).toBe(200);
+        expect(response.body.roomList[1].RoomNumber).toBe(1)
+        expect(response.body.roomList[1].RoomType).toBe("unitTest")
+        expect(response.body.roomList[1].RoomInfo).toBe("unitTest")
+        expect(response.body.roomList[1].UniID).toBe("6566be76dd46c9561a7f8419")
+        expect(response.body.roomList[0].RoomNumber).toBe(0)
+        expect(response.body.roomList[0].RoomType).toBe("unitTest")
+        expect(response.body.roomList[0].RoomInfo).toBe("unitTest")
+        expect(response.body.roomList[0].UniID).toBe("6566be76dd46c9561a7f8419")
+
+    })
+
 
     // Other CRUD operations tests
 });
